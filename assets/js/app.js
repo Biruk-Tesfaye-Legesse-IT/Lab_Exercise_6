3f860ef0767b97452bce4c0eab5f6fe6b4a33053
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-02-04 21:21:31 +0300

    Sorting algorithm is inserted.

--- assets/js/app.js
+++ assets/js/app.js
@@ -112,6 +112,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ****************************************
 
+    function bubbleSortD(arr) {
+        var len = arr.length;
+        for (var i = len - 1; i >= 0; i--) {
+            for (var j = 1; j <= i; j++) {
+                if (arr[j - 1].taskDate < arr[j].taskDate) {
+                    var temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
 
 
     // ======================================== Display Task ==========================================
